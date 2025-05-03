9896cc88a20c89c15eb508ddbfa2e2cc8c557d99
armiinygh <seyedalirezayaghobi@outlook.com>
2025-05-03 12:55:15 +0200

    Add testing evaluation to the plot

--- src/Config/config.py
+++ src/Config/config.py
@@ -3,14 +3,14 @@ from torch import nn
 
 cfg = SimpleNamespace(**{})
 
-cfg.epochs = 5
+cfg.epochs = 20
 cfg.batch_size = 128
 cfg.learning_rate = 0.00001
 cfg.output_size = 256
 
 
 
-cfg.loss_function = nn.NLLLoss() #[nn.CrossEntropyLoss(),  nn.NLLLoss(), ]
+cfg.loss_function = nn.CrossEntropyLoss() #[nn.CrossEntropyLoss(),  nn.NLLLoss(), ]
 cfg.activation_function = nn.ReLU() #[nn.LeakyReLU() , nn.GELU(), nn.ELU()]
 # run config
 cfg.save_model = True
