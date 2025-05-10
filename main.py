9ea522b0ff9a64e21f6ba54069dfede33e695896
armiinygh <seyedalirezayaghobi@outlook.com>
2025-05-10 13:31:39 +0200

    Add Deep NN

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys 
 sys.path.append('src')
-from Shallow_nn.Shallow_nn import train_model
+from Shallow_nn.Shallow_nn import train_shallow_model
 from Config.config import cfg
-
+from Deep_nn.Deep_nn import train_deep_model
 
 if __name__ == "__main__" :
-    train_model()
+    train_shallow_model()
+    train_deep_model()
